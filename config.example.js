d9c126df3914c06f96b3d2910c4f4c471c345a01
Souseiseki <mail@dimius.ru>
2024-10-01 09:56:24 +0000

    Пример конфига

---
+++ config.example.js
@@ -0,0 +1,17 @@
+export default {
+    port: '3000',
+    minecraft: {
+        server: {
+            domain: 'cmcrafts.su',
+            ip: '94.26.229.202',
+            port: '25565',
+            versions: ['1.21', '1.21.1']
+        }
+    },
+    database: {
+        host: 'localhost',
+        database: 'cmc',
+        user: 'nikhokdangd',
+        password: 'bobr123'
+    }
+}
