18c8ba373e7398e926523d4425a8b8f1f3929f8b
Souseiseki <mail@dimius.ru>
2024-10-04 14:10:27 +0300

    Пояснения к конфигу

--- config.example.js
+++ config.example.js
@@ -1,14 +1,18 @@
 export default {
+    // Порт веб-сервера
     port: '3000',
     minecraft: {
+        // Параметры майнкрафт-сервера
         server: {
             domain: 'cmcrafts.su',
             ip: '94.26.229.202',
             port: '25565',
             versions: ['1.21', '1.21.1']
         },
+        // Мониторинг (для получения аптайма)
         hotmc: 'https://hotmc.ru/minecraft-server-235548'
     },
+    // Данные для подключения к MySQL (нужно создать БД вручную)
     database: {
         host: 'localhost',
         database: 'cmc',
