5cfca8f077b5d39b1220e06a07d9941af6c356bc
Souseiseki <mail@dimius.ru>
2024-10-21 13:46:29 +0300

    Убран лишний цикл

--- server/routes/pages/read.js
+++ server/routes/pages/read.js
@@ -4,20 +4,13 @@ const read = Router()
 import fs from 'fs'
 import md from "#lib/md"
 
-const html = { }
-
 read.get('/:url', (req, res) => {
 
-    fs.readdirSync('public/read').forEach(file => {
-        const page = file.replace('.md', '');
-        html[page] = md.read.render(fs.readFileSync(`public/read/${file}`, 'utf-8'));
-    });
-	
-	const url = req.params.url
+	const html = md.read.render(fs.readFileSync(`public/read/${req.params.url}.md`, 'utf-8'));
 	
 	res.render('pages/read', {
 		req,
-		text: html[url]
+		text: html
 	})
 
 })
