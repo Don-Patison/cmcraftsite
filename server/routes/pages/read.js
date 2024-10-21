63ee20f7d32ce2775dbff6b1336b3f951d3ad58c
Souseiseki <mail@dimius.ru>
2024-10-21 14:14:03 +0300

    Добавлена возможность использовать EJS в /read

--- server/routes/pages/read.js
+++ server/routes/pages/read.js
@@ -3,14 +3,16 @@ const read = Router()
 
 import fs from 'fs'
 import md from "#lib/md"
+import ejs from "ejs"
 
 read.get('/:url', (req, res) => {
 
 	const html = md.read.render(fs.readFileSync(`public/read/${req.params.url}.md`, 'utf-8'));
+	const injected = ejs.render(html.replaceAll('&lt;', '<').replaceAll('&gt;', '>'), { req, ...res.locals })
 	
 	res.render('pages/read', {
 		req,
-		text: html
+		text: injected
 	})
 
 })
